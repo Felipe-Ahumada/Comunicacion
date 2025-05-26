847780b59dabceb2821e61487d1cc29946454651
Felipe-Ahumada <felipe.ahumada.silva2001@gmail.com>
2025-05-26 10:22:58 -0400

    Se ha agregado http Status a foro

--- src/main/java/com/edutech/edutech/controller/ForoController.java
+++ src/main/java/com/edutech/edutech/controller/ForoController.java
@@ -3,6 +3,8 @@ package com.edutech.edutech.controller;
 import com.edutech.edutech.model.Foro;
 import com.edutech.edutech.service.ForoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,22 +17,34 @@ public class ForoController {
     private ForoService foroService;
 
     @PostMapping
-    public Foro crearForo(@RequestBody Foro foro) {
-        return foroService.crearForo(foro);
+    public ResponseEntity<Foro> crearForo(@RequestBody Foro foro) {
+        Foro creado = foroService.crearForo(foro);
+        return new ResponseEntity<>(creado, HttpStatus.CREATED);
     }
 
     @GetMapping
-    public List<Foro> listarForos() {
-        return foroService.obtenerTodosLosForos();
+    public ResponseEntity<List<Foro>> listarForos() {
+        List<Foro> foros = foroService.obtenerTodosLosForos();
+        return foros.isEmpty()
+            ? new ResponseEntity<>(HttpStatus.NO_CONTENT)
+            : new ResponseEntity<>(foros, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public Foro obtenerPorId(@PathVariable Long id) {
-        return foroService.obtenerForoPorId(id);
+    public ResponseEntity<Foro> obtenerPorId(@PathVariable Long id) {
+        Foro foro = foroService.obtenerForoPorId(id);
+        return foro == null
+            ? new ResponseEntity<>(HttpStatus.NOT_FOUND)
+            : new ResponseEntity<>(foro, HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
-    public void eliminar(@PathVariable Long id) {
+    public ResponseEntity<Void> eliminar(@PathVariable Long id) {
+        Foro foro = foroService.obtenerForoPorId(id);
+        if (foro == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
         foroService.eliminarForo(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
